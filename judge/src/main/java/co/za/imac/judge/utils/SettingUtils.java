545708697a14cf4c3bab9cbcfc003b9767eeb65b
Tej Pillay <tejpillay@gmail.com>
2023-04-19 20:45:40 +0200

    added changes

--- judge/src/main/java/co/za/imac/judge/utils/SettingUtils.java
+++ judge/src/main/java/co/za/imac/judge/utils/SettingUtils.java
@@ -2,7 +2,7 @@ package co.za.imac.judge.utils;
 
 public class SettingUtils {
     
-    public static final String APPLICATION_CONFIG_PATH = "/tmp";
+    public static final String APPLICATION_CONFIG_PATH = "/var/opt/judge";
 
 
 }
