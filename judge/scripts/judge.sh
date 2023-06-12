b312ea9eb5bbf8215817ea6de89efe5b59853d56
Rolf Bartels <rolfbartels@outlook.com>
2023-06-12 12:15:03 +0200

    Clean Up

--- judge/scripts/judge.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/java -jar /var/opt/judge/bin/judge.jar > /var/opt/judge/judge.log
--- judge/scripts/judge_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-systemctl stop judge.service
-systemctl stop kiosk.service
-
-mv judge-0.0.1-SNAPSHOT.jar /var/opt/judge/bin/judge.jar
-
-systemctl start judge.service
-systemctl start kiosk.service
