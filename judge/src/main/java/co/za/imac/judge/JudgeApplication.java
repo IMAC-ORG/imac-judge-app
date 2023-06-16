85c6588009f91125aec8d55997a454063cca5bfd
Dan Carroll <dan@carroll.id.au>
2023-06-16 14:55:35 +1000

    - Run time ability to set config location : judge.config.path   App also checks for CWD/settings.json and will use that   if present. - Added copy-rename-maven-plugin plugin to copy built jar to   dockerfile directory to build image.

--- judge/src/main/java/co/za/imac/judge/JudgeApplication.java
+++ judge/src/main/java/co/za/imac/judge/JudgeApplication.java
@@ -1,14 +1,46 @@
 package co.za.imac.judge;
 
+import co.za.imac.judge.utils.SettingUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.File;
 
 @SpringBootApplication
 public class JudgeApplication {
+	private static final Logger logger =
+			LoggerFactory.getLogger(JudgeApplication.class);
 
 	public static void main(String[] args) {
+		checkAppConfigDirOverrides();
 		SpringApplication.run(JudgeApplication.class, args);
 	}
 
-	
+	/**********************
+	 * Checks if settings.json exists in the CWD or if a directory is provided specifically as a property.
+	 * If specifically provided then that takes preference.
+	 * Otherwise if it's in the CWD then that's next.
+	 * Finally it defaults to the standard location.
+	 */
+	private static void checkAppConfigDirOverrides() {
+
+		String AppConfigPath = System.getProperty("judge.config.path");
+		if (AppConfigPath == null) {
+			String userDirectory = System.getProperty("user.dir");
+			File f = new File(userDirectory + File.separator + "settings.json");
+			if(f.exists() && !f.isDirectory()) {
+				logger.info("Settings file found - setting Application Config path to Current Working Dir.");
+				SettingUtils.setApplicationConfigPath(userDirectory);
+			}
+		} else {
+			File appConfigDir = new File (AppConfigPath);
+			if (appConfigDir.exists() && appConfigDir.isDirectory()) {
+				logger.info("Setting Applicatication Config path to " + AppConfigPath + ".");
+				SettingUtils.setApplicationConfigPath(AppConfigPath);
+			} else {
+				logger.error("Application Config path:" + AppConfigPath + " does not exist.  Refusing to use it.");
+			}
+		}
+	}
 }
