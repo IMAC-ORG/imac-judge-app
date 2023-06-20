9f63215329ecb832e0311df04a45dca4b78589cb
davecw <davecw@ec.rr.com>
2023-06-19 22:20:44 -0400

    Added check for secondary_id exists before checking length

--- judge/src/main/java/co/za/imac/judge/service/PilotService.java
+++ judge/src/main/java/co/za/imac/judge/service/PilotService.java
@@ -102,7 +102,8 @@ public class PilotService {
                 int primary_id = Integer.parseInt(element.getElementsByTagName("primary_id").item(0).getTextContent());
                 int secondary_id = 0;
                 int comp_id = 0;
-                if (element.getElementsByTagName("secondary_id").item(0).getTextContent().length() > 0) {
+                if (element.getElementsByTagName("secondary_id").item(0).hasChildNodes() == true && 
+                    element.getElementsByTagName("secondary_id").item(0).getTextContent().length() > 0) {
                     secondary_id = Integer
                             .parseInt(element.getElementsByTagName("secondary_id").item(0).getTextContent());
                 }
