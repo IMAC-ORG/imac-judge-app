6bdff945c281a842d052d645a3dada2be4416b7f
Rolf Bartels <rolfbartels@outlook.com>
2023-06-28 11:44:58 +0000

    Caller calls next figure on page change

--- scripts/judge_update.sh
+++ scripts/judge_update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-systemctl stop judge.service
-systemctl stop kiosk.service
+sudo systemctl stop judge.service
+sudo systemctl stop kiosk.service
 
-sudo wget -O /var/opt/judge/bin/judge.jar http://judge.jar
+sudo wget -O /var/opt/judge/bin/judge.jar https://drive.google.com/file/d/1dkYaoU-6ueR7LCX1nh9Kpic7iX_rgmsH/view?usp=drive_link
 
 sudo chmod 777 -R /var/opt/judge
 
-systemctl start judge.service
-systemctl start kiosk.service
+sudo systemctl start judge.service
+sudo systemctl start kiosk.service
