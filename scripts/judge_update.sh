ffa3a0d643fdd21d4b4b958817db15258f3127e6
Rolf Bartels <rolfbartels@outlook.com>
2023-06-28 11:55:29 +0000

    judge_update-script

--- scripts/judge_update.sh
+++ scripts/judge_update.sh
@@ -2,8 +2,8 @@
 sudo systemctl stop judge.service
 sudo systemctl stop kiosk.service
 
-sudo wget -O /var/opt/judge/bin/judge.jar https://drive.google.com/file/d/1dkYaoU-6ueR7LCX1nh9Kpic7iX_rgmsH/view?usp=drive_link
-
+#sudo wget -O /var/opt/judge/bin/judge.jar https://drive.google.com/file/d/1dkYaoU-6ueR7LCX1nh9Kpic7iX_rgmsH/view?usp=drive_link
+sudo mv /home/judge/judge*.jar /var/opt/judge/bin/judge.jar
 sudo chmod 777 -R /var/opt/judge
 
 sudo systemctl start judge.service
