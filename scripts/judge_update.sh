1af3dc20e671524f6f469ae2bd6beabf84c7c545
Rolf Balrtels <rolfbartels@outlook.com>
2023-07-19 14:30:26 +0200

    Update judge_update.sh

--- scripts/judge_update.sh
+++ scripts/judge_update.sh
@@ -2,7 +2,7 @@
 sudo systemctl stop judge.service
 sudo systemctl stop kiosk.service
 
-sudo wget -O /var/opt/judge/bin/judge.jar https://github.com/IMAC-ORG/imac-judge-app/releases/latest/download/judge.jar
+sudo wget -O /var/opt/judge/bin/judge.jar https://github.com/IMAC-ORG/imac-judge-app/releases/download/v1.3/judge.jar
 sudo chmod 777 -R /var/opt/judge
 
 sudo systemctl start judge.service
