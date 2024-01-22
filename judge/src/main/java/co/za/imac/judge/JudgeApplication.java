a6ecc949db2063dad75983042ca6561cccb13b81
davecw <davecw@ec.rr.com>
2024-01-21 19:29:10 -0500

    Moved images and mp3 out of judge, improvements to docker and devcontainer, run as judge user

--- judge/src/main/java/co/za/imac/judge/JudgeApplication.java
+++ judge/src/main/java/co/za/imac/judge/JudgeApplication.java
@@ -5,11 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.io.File;
-import java.util.Properties;
 
 @SpringBootApplication
-public class JudgeApplication {
+public class JudgeApplication implements WebMvcConfigurer {
 	private static final Logger logger =
 			LoggerFactory.getLogger(JudgeApplication.class);
 
@@ -44,4 +46,13 @@ public class JudgeApplication {
 			}
 		}
 	}
+
+	//sets the path /man to the location where we now store outside of the jar
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/man/**").addResourceLocations("file://" + SettingUtils.getApplicationConfigPath() + "/figures/")
+                .setCachePeriod(0);
+		
+    }
+	
 }
